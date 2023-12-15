3b257a21cf168f724e17c2507d7278a8752bbcbe
YOSUKEHANAOKA <yhana0921@gmail.com>
2023-12-16 00:22:22 +0900

    added api file uploader

---
+++ api_file_uploader/app.py
@@ -0,0 +1,12 @@
+# How to Run this App:
+# cd api_file_uploader
+# pipenv run streamlit run app.py
+
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
